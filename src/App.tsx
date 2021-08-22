ae4c3cb24b124edf4296664b77904fb9ca6f6619
chitho <charleskch@hotmail.com>
2021-08-22 18:45:08 +0800

    change js files to tsx files

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react'
 
 import firebase from "firebase/app"
 import "firebase/database"
@@ -42,35 +40,22 @@ function App() {
     if (!db) return
 
     const dbRef = db.ref()
-    dbRef.child("test").get().then((snapshot) => {
+    dbRef.child("test").get().then((snapshot: any) => {
       if (snapshot.exists()) {
-        console.log('get data success', snapshot.val());
+        console.log('get data success', snapshot.val())
       } else {
-        console.log("No data available");
+        console.log("No data available")
       }
-    }).catch((error) => {
-      console.error(error);
-    });
+    }).catch((error: Error) => {
+      console.error(error)
+    })
   })
   
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello world
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
