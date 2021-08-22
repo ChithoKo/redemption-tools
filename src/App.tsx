4adc99eb1cce4cfabb4c355cc1ddd2b10d35f625
chitho <charleskch@hotmail.com>
2021-08-22 18:45:08 +0800

    change js files to tsx files

--- src/App.tsx
+++ src/App.tsx
@@ -5,32 +5,19 @@ import "firebase/database"
 
 function App() {
   const [db, setDb] = useState<any>(null)
-  const [username, setUsername] = useState<string>("")
-
-  const config = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSENGE_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
-  }
-
-  const handleInputChange = (event: any) => {
-    setUsername(event.target.value)
-  }
-  const createUser = () => {
-    console.log('create new user', username)
-    db.ref('users').set({
-      1: {
-        username: username
-      }
-    })
-  }
 
   useEffect(() => {
+    const config = {
+      apiKey: "AIzaSyDOgifivnGB0p7YWUPsuXKaad6UNvgYGZo",
+      authDomain: "redemption-tools.firebaseapp.com",
+      databaseURL: "https://redemption-tools-default-rtdb.asia-southeast1.firebasedatabase.app",
+      projectId: "redemption-tools",
+      storageBucket: "redemption-tools.appspot.com",
+      messagingSenderId: "320014175119",
+      appId: "1:320014175119:web:3c243ab8f94213d7c319fa",
+      measurementId: "G-KS5T9XH709"
+    }
+
     firebase.initializeApp(config)
     const db = firebase.database()
     setDb(db)
