974a6766789040a669ed95aaf4443c477db8cfc1
chitho <charleskch@hotmail.com>
2021-08-27 20:27:28 +0800

    [Refactor] move firebase configs to constant and clean some App.tsx codes

--- src/App.tsx
+++ src/App.tsx
@@ -2,54 +2,32 @@ import { useEffect, useState } from 'react'
 
 import firebase from "firebase/app"
 import "firebase/database"
+import { configs as firebaseConfigs } from './constants/firebase'
 
+interface User {
+  username: string
+}
 function App() {
   const [db, setDb] = useState<any>(null)
   const [username, setUsername] = useState<string>("")
-
-  const config = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSENGE_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
-  }
 
   const handleInputChange = (event: any) => {
     setUsername(event.target.value)
   }
   const createUser = () => {
-    console.log('create new user', username)
-    db.ref('users').set({
-      1: {
-        username: username
-      }
+    db.ref('users').push({
+      username: username
     })
   }
+  const isFirebaseInitialized = (): boolean => firebase.apps.length !== 0
 
   useEffect(() => {
-    firebase.initializeApp(config)
-    const db = firebase.database()
-    setDb(db)
+    if (!isFirebaseInitialized()) {
+      firebase.initializeApp(firebaseConfigs)
+    }
+    const firebaseDB = firebase.database()
+    setDb(firebaseDB)
   }, [])
-
-  useEffect(() => {
-    if (!db) return
-
-    const dbRef = db.ref()
-    dbRef.child("test").get().then((snapshot: any) => {
-      if (snapshot.exists()) {
-        console.log('get data success', snapshot.val())
-      } else {
-        console.log("No data available")
-      }
-    }).catch((error: Error) => {
-      console.error(error)
-    })
-  })
   
   return (
     <div className="App">
