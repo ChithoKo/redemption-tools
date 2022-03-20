e13de33a4057c6be83504c08ea124356028bdb3d
Charles Ko <charles.ko@decathlon.com>
2022-03-20 15:25:00 +0800

    feat: add redemptionType as user attribute

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,31 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, ChangeEventHandler, ChangeEvent } from 'react'
 import { db as firebaseDB } from '../services/firebase-client'
 
 interface User {
-  username: string
+  username: string,
+  redemptionType: string,
+  createDate: Date
 }
 
 export const HomePage = () => {
-  const [username, setUsername] = useState<string>("")
+  const [username, setUsername] = useState("")
+  const [redemptionType, setRedemptionType] = useState("")
   const [users, setUsers] = useState<User[]>([])
 
-  const handleInputChange = (event: any) => {
-    setUsername(event.target.value)
+  const propsSetter: { [key: string]: ChangeEventHandler<HTMLInputElement> }= {
+    "username": ((e: ChangeEvent<HTMLInputElement>) => {setUsername(e.target.value)}),
+    "redemptionType": ((e: ChangeEvent<HTMLInputElement>) => {setRedemptionType(e.target.value)}),
+  }
+  const createInputChangeHandler = (name: string): ChangeEventHandler<HTMLInputElement> => {
+    const handleInputChange = propsSetter[name] || ((event: ChangeEvent<HTMLInputElement>) => {})
+
+    return handleInputChange
   }
   const createUser = async () => {
     await firebaseDB.append('users', {
-      username: username
+      username,
+      redemptionType,
+      createDate: new Date()
     })
 
     fetchUsers()
@@ -31,12 +42,13 @@ export const HomePage = () => {
 
   return (
     <>
-      <input type="text" name="username" value={ username } onChange={ handleInputChange } />
+      <input type="text" name="username" value={ username } onChange={ createInputChangeHandler('username') } />
+      <input type="text" name="redemptionType" value={ redemptionType } onChange={ createInputChangeHandler('redemptionType') } />
       <button onClick={ createUser }>Create</button>
       <ul>
         {
           users.map((user: User, idx: number) => (
-            <li key={idx}>{ user.username }</li>
+            <li key={idx}>{ user.username } - { user.redemptionType }</li>
           )) 
         }
       </ul>
